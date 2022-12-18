78831514471ea540270652cffdbf81fba575c45c
KBindesh <test@email.com>
2022-12-19 02:39:11 +0530

    US-Updated Provisioners

--- Module-08_Terraform_Modules/02_Custom_Modules/tf-variables.tf
+++ Module-08_Terraform_Modules/02_Custom_Modules/tf-variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "my_s3_bucket" {
   description = "S3 Bucket name that we pass to S3 Custom Module"
   type = string
-  default = "bindeshs3webbucket"
+  default = "novanewstaticwebsite"
 }
 
 ## Create Variable for S3 Bucket Tags
--- Module-10_Terraform_Remote_State_Management/tf-settings.tf
+++ Module-10_Terraform_Remote_State_Management/tf-settings.tf
@@ -1,14 +1,19 @@
-terraform {
+terraform {  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.47.0"
     }
   }
+  backend "s3" {
+    bucket = "novatfbackendbucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
+  region  = "us-east-1"
   profile = "default"
 }
